9245bc2b7ccdbb39f70a79a0e28f36d61f943c23
Tani Umetsu <taniu@hawaii.edu>
2023-09-26 13:26:43 -1000

    Completed parts 1-9

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,27 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+//Part3
+var fruitNames : [String]
+
+//Part4
+fruitNames = ["Kiwi", "Apples", "Watermelon"]
+
+//Part5
+print(fruitNames)
+
+//Part6
+print("I like to eat \(fruitNames)")
+
+//Part7
+for name in fruitNames
+{
+    print("I like to eat " + name)
+}
+
+//Part8
+print(fruitNames[1])
+
+//Part9
+
